a8f7b7621bea89ae115363c943ec384532c33bb0
gbasibabu <gbasibabu@gmail.com>
2016-01-24 02:45:03 -0800

    Compute, Cache and Return inverse of a matrix
    This program calculates the inverse of a matrix, cached it into the
    memory and if the request for same matrix comes, it returns the reverse
    from cache rather than computing every time

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This two functions are implemented to compute,cache and retrieve the inverse of a matrix
 
 ## Write a short comment describing this function
+#This function saves the copy of inverse matrix into cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
+#This function is to find the reverse matrix from cache, if not calcualte inverse and save it 
+#to cache before return the same
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
